684db47b962247c4f68655d8680fed5450fb9eda
Jesse Stricker <me@jessestricker.de>
2020-02-12 14:12:33 +0100

    Add example for source location

---
+++ examples/source_location.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <meta/meta.hpp>
+
+void do_log(const meta::SourceLocation& loc, const char* text) {
+  std::cout << loc << ": " << text << '\n';
+}
+
+#define LOG(text__) do_log(META_CURRENT_SOURCE_LOCATION, text__)
+
+int main() {
+  const auto def_src_loc = meta::SourceLocation{};
+  std::cout << def_src_loc << '\n';
+  std::wcout << def_src_loc << '\n';
+  std::cout << '\n';
+
+  const auto src_loc = META_CURRENT_SOURCE_LOCATION;
+  std::cout << src_loc << '\n';
+  std::wcout << src_loc << '\n';
+  std::cout << '\n';
+
+  LOG("example log message");
+}
