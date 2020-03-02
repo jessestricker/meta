e4ba165936ead09e25e86a593fdaf79c31404f1a
Jesse Stricker <me@jessestricker.de>
2020-03-03 00:47:36 +0100

    Add tests for Version

--- test/source_location.cpp
+++ test/source_location.cpp
@@ -1,17 +1,9 @@
 #include <meta/source_location.hpp>
-#include <sstream>
 
 #include "catch/catch.hpp"
+#include "catch/helpers.hpp"
 
 using namespace Catch::Matchers;
-
-namespace {
-  std::string from_stream_to_string(const meta::SourceLocation& obj) {
-    auto oss = std::ostringstream{};
-    oss << obj;
-    return oss.str();
-  }
-}
 
 TEST_CASE("SourceLocation: default constructor") {
   const auto loc = meta::SourceLocation{};
@@ -28,7 +20,7 @@ TEST_CASE("SourceLocation: explicit constructor") {
 TEST_CASE("SourceLocation: macro constructor") {
   const auto loc = META_CURRENT_SOURCE_LOCATION;
   REQUIRE_THAT(std::string{loc.file()}, EndsWith("source_location.cpp"));
-  REQUIRE(loc.line() == 29);
+  REQUIRE(loc.line() == 21);
 }
 
 TEST_CASE("SourceLocation: to_string and ostream operator") {
